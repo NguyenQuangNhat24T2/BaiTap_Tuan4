997b947299da59a094e9cbd4d5ce0916d141c781
Nguyen Quang Nhat <nguyenquangnhat200806@gmail.com>
2025-09-19 21:32:43 +0700

    Đã hoàn thành bài 8 của Nhựt nhaa

---
+++ Bai8_QuangNhat.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+public class Bai8_QuangNhat 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so phan tu n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Nhap cac phan tu cua mang:");
+        for (int i = 0; i < n; i++) 
+            arr[i] = sc.nextInt();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int x : arr) 
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        int maxFreq = 0;
+        int mode = arr[0]; 
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) 
+        {
+            if (entry.getValue() > maxFreq) 
+            {
+                maxFreq = entry.getValue();
+                mode = entry.getKey();
+            }
+        }
+        System.out.println("Phan tu xuat hien nhieu nhat: " + mode + " (xuat hien " + maxFreq + " lan)");
+        sc.close();
+    }
+}
