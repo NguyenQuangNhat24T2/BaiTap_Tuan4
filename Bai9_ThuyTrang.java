1d4d692a6692d6d12a4d888a080959fcaf741b8a
Dothithuytrang2142 <dtthuytrang29@gmail.com>
2025-09-21 00:14:56 +0700

    Them bai 9

---
+++ Bai9_ThuyTrang.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bai9_ThuyTrang {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        System.out.println("Nhap danh sach so nguyen (nhap -1 de ket thuc):");
+        
+        while (true)
+         {
+            System.out.print("Nhap so: ");
+            int so = scanner.nextInt();
+            
+            if (so == -1) {
+                break;
+            }
+            
+            list.add(so);
+        }
+        
+        System.out.println("\nDanh sach so vua nhap:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("So " + (i + 1) + ": " + list.get(i));
+        }
+        
+        scanner.close();
+    }
+}
