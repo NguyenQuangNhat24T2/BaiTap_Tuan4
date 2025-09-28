3aba88ab76f55db0bd6168ede46f533121c1d522
NguyenQuangNhat24T2 <nguyenquangnhat200806@gmail.com>
2025-09-28 20:41:57 +0700

    Update and rename Bai4_QuangNhat.java to Bai4.java
    đổi tên

--- Bai4.java
+++ Bai4.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-public class Bai4_QuangNhat {
+public class Bai4 
+{
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap n: ");
         int n = sc.nextInt();
-        int tong1 = 0;
+        int tong = 0;
         for (int i = 1; i <= n; i++) 
-            tong1 += i;
-        System.out.println("Tong = " + tong1);
+            tong += i;
+        System.out.println("Tong = " + tong);
         sc.close();
     }
 }
